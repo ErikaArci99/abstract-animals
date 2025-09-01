f9aaa7914ac6030bd8189f91686b917e2a667a9b
ErikaArci99 <erikaarci1999@outlook.it>
2025-09-01 18:43:06 +0200

    Add common methods to Abstractanimals class

--- src/org/lessons/animals/Abstractanimals.java
+++ src/org/lessons/animals/Abstractanimals.java
@@ -2,4 +2,13 @@ package org.lessons.animals;
 
 public abstract class Abstractanimals {
 
+    // metodo comune a tutti gli animali
+    public void dormi() {
+        System.out.println("Zzz");
+    }
+
+    // metodi da implementare nelle sottoclassi
+    public abstract void verso();
+
+    public abstract void mangia();
 }
